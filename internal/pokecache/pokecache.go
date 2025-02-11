0edf4d8b4cf5b9bb77e228ab807a76cb319adb59
benKapl <benji1.kaplan@gmail.com>
2025-02-11 17:52:12 +0100

    added tests for pokecache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,8 +1,30 @@
 package pokecache
 
 import (
+	"sync"
 	"time"
 )
+
+type (
+	cacheEntry struct {
+		createdAt time.Time
+		val       []byte
+	}
+	Cache struct {
+		cache map[string]cacheEntry
+		mu    *sync.Mutex
+	}
+)
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mu:    &sync.Mutex{},
+	}
+
+	go c.reapLoop(interval)
+	return c
+}
 
 func (c *Cache) Add(key string, val []byte) {
 	c.mu.Lock()
